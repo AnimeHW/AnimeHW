e25ba9312d9d1342d268836a85a4e3ff066702fb
Shadizinho <163308924+AnimeHW@users.noreply.github.com>
2024-04-11 18:58:59 -0300

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,12 +6,12 @@ class Config(object):
     sudo_users = "7080925947", "6959003848"
     GROUP_ID = -1002079755812
     TOKEN = "6782667556:AAGImAWRJbss9BXEpALCjisaxYVMC7sKUt0"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://Shadizinho:Ggae4304,,@animehw.kdajkvp.mongodb.net/"
     PHOTO_URL = ["https://telegra.ph/file/9bfd5dfc5633040cd8b8f.jpg", "https://telegra.ph/file/f2750be308765b83b3937.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
+    SUPPORT_CHAT = "AnimeHW_sup"
+    UPDATE_CHAT = "AnimeHW_sup"
     BOT_USERNAME = "AnimeHW_bot"
-    CHARA_CHANNEL_ID = "-1002133191051"
+    CHARA_CHANNEL_ID = "-1002028837181"
     api_id = 21732614
     api_hash = "ff91cccbb310e68d0b93320abb2d229d"
 
